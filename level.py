0d922ef5ead5afd5f667e3f2ce7c14e4690bb988
grebson <grebciu@gmail.com>
2022-02-06 00:45:50 +0100

    Another commit

--- level.py
+++ level.py
@@ -29,7 +29,7 @@ class Level:
 
     def run(self):
         # update and draw the game
-        self.visible_sprites.custom_draw()
+        self.visible_sprites.custom_draw(self.player)
         self.visible_sprites.update()
 
 class YSortCameraGroup(pygame.sprite.Group):
@@ -37,9 +37,15 @@ class YSortCameraGroup(pygame.sprite.Group):
         # general setup
         super().__init__()
         self.display_surface = pygame.display.get_surface()
+        self.half_width = self.display_surface.get_size()[0] // 2
+        self.half_height = self.display_surface.get_size()[1] // 2
         self.offset = pygame.math.Vector2(100,200)
 
-    def custom_draw(self):
+    def custom_draw(self, player):
+        # getting the offset
+        self.offset.x = player.rect.centerx - self.half_width
+        self.offset.y = player.rect.centery - self.half_height
+
         for sprite in self.sprites():
-            offset_pos = sprite.rect.topleft + self.offset
+            offset_pos = sprite.rect.topleft - self.offset
             self.display_surface.blit(sprite.image, offset_pos)
