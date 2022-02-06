512eac5eea9c3e875fd166a945bd36499f3444e4
grebson <grebciu@gmail.com>
2022-02-06 09:34:23 +0100

    Another commit

--- level.py
+++ level.py
@@ -41,10 +41,18 @@ class YSortCameraGroup(pygame.sprite.Group):
         self.half_height = self.display_surface.get_size()[1] // 2
         self.offset = pygame.math.Vector2(100,200)
 
+        # creating the floor
+        self.floor_surface = pygame.image.load('./images/tilemap/ground.png')
+        self.floor_rect = self.floor_surface.get_rect(topleft = (0,0))
+
     def custom_draw(self, player):
         # getting the offset
         self.offset.x = player.rect.centerx - self.half_width
         self.offset.y = player.rect.centery - self.half_height
+
+        # drawing the floor
+        floor_offset_pos = self.floor_rect.topleft - self.offset
+        self.display_surface.blit(self.floor_surface, floor_offset_pos)
 
         # for sprite in self.sprites():
         for sprite in sorted(self.sprites(), key = lambda sprite: sprite.rect.centery):
