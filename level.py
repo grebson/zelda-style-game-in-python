ad2c43701c482c219c166b47771b3f9db164c3c5
grebson <grebciu@gmail.com>
2022-02-08 18:35:59 +0100

    Another commit

--- level.py
+++ level.py
@@ -5,6 +5,7 @@ from player import Player
 from debug import debug
 from support import *
 from random import choice
+from weapon import Weapon
 
 class Level:
     def __init__(self):
@@ -46,7 +47,10 @@ class Level:
                             surface = images['objects'][int(col)]
                             Tile((x,y), [self.visible_sprites, self.obstacle_sprites], 'object', surface)
 
-        self.player = Player((2000,1430), [self.visible_sprites], self.obstacle_sprites)
+        self.player = Player((2000,1430), [self.visible_sprites], self.obstacle_sprites, self.create_attack)
+
+    def create_attack(self):
+        Weapon(self.player, [self.visible_sprites])
 
     def run(self):
         # update and draw the game
--- settings.py
+++ settings.py
@@ -1,4 +1,14 @@
+# game setup
 WIDTH = 1280
 HEIGHT = 720
 FPS = 60
 TILESIZE = 64
+
+# weapons
+weapon_data = {
+    'sword': {'cooldown': 100, 'damage': 15, 'image': './images/weapons/sword/full.png'},
+    'lance': {'cooldown': 400, 'damage': 30, 'image': './images/weapons/lance/full.png'},
+    'axe': {'cooldown': 300, 'damage': 20, 'image': './images/weapons/axe/full.png'},
+    'rapier': {'cooldown': 50, 'damage': 8, 'image': './images/weapons/rapier/full.png'},
+    'sai': {'cooldown': 80, 'damage': 10, 'image': './images/weapons/sai/full.png'}
+}
