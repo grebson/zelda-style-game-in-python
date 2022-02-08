6621aa248d714520e94ee42980d5f18ebf9105c1
grebson <grebciu@gmail.com>
2022-02-08 19:13:24 +0100

    Another commit

--- player.py
+++ player.py
@@ -29,6 +29,9 @@ class Player(pygame.sprite.Sprite):
         self.destroy_attack = destroy_attack
         self.weapon_index = 0
         self.weapon = list(weapon_data.keys())[self.weapon_index]
+        self.can_switch_weapon = True
+        self.weapon_switch_time = None
+        self.switch_duration_cooldown = 200
 
     def import_player_assets(self):
         character_path = './images/player/'
@@ -86,6 +89,17 @@ class Player(pygame.sprite.Sprite):
                 self.attack_time = pygame.time.get_ticks()
                 print('do magic stuff')
 
+            if keys[pygame.K_q] and self.can_switch_weapon:
+                self.can_switch_weapon = False
+                self.weapon_switch_time = pygame.time.get_ticks()
+
+                if self.weapon_index < len(list(weapon_data.keys())) - 1:
+                    self.weapon_index += 1
+                else:
+                    self.weapon_index = 0
+            
+            self.weapon = list(weapon_data.keys())[self.weapon_index]
+
     def get_status(self):
         # idle status
         if self.direction.x == 0 and self.direction.y == 0:
@@ -139,6 +153,10 @@ class Player(pygame.sprite.Sprite):
             if current_time - self.attack_time >= self.attack_cooldown:
                 self.attacking = False
                 self.destroy_attack()
+
+        if not self.can_switch_weapon:
+            if current_time - self.weapon_switch_time >= self.switch_duration_cooldown:
+                self.can_switch_weapon = True
 
     def animate(self):
         animation = self.animations[self.status]
