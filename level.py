376422570f65f1ce2eef36a4c1e113b127ea1ccd
grebson <grebciu@gmail.com>
2022-02-09 16:58:41 +0100

    Another commit

--- level.py
+++ level.py
@@ -69,7 +69,7 @@ class Level:
         # update and draw the game
         self.visible_sprites.custom_draw(self.player)
         self.visible_sprites.update()
-        debug(self.player.status)
+        #debug(self.player.status)
         self.ui.display(self.player)
 
 class YSortCameraGroup(pygame.sprite.Group):
