1fa516dd6fbb7d261ff70e7daaa2a2c95882374e
grebson <grebciu@gmail.com>
2022-02-16 17:00:13 +0100

    Another commit

--- level.py
+++ level.py
@@ -5,7 +5,7 @@ from tile import Tile
 from player import Player
 from debug import debug
 from support import *
-from random import choice
+from random import choice, randint
 from weapon import Weapon
 from ui import UI
 from enemy import Enemy
@@ -95,7 +95,9 @@ class Level:
                     for target_sprite in collision_sprites:
                         if target_sprite.sprite_type == 'grass':
                             pos = target_sprite.rect.center
-                            self.animation_player.create_grass_particles(pos, [self.visible_sprites])
+                            offset = pygame.math.Vector2(0,75)
+                            for leaf in range(randint(3,6)):
+                                self.animation_player.create_grass_particles(pos - offset, [self.visible_sprites])
                             target_sprite.kill()
                         else:
                             target_sprite.get_damage(self.player, attack_sprite.sprite_type)
