9b8663faa2b555ed6b8d2f1fbb649308f45cbc55
grebson <grebciu@gmail.com>
2022-02-16 17:24:10 +0100

    Another commit

---
+++ magic.py
@@ -0,0 +1,12 @@
+import pygame
+from settings import *
+
+class MagicPlayer:
+    def __init__(self, animation_player):
+        self.animation_player = animation_player
+    
+    def heal(self):
+        print('heal')
+
+    def flame(self):
+        pass
