80dfa5f3601553d4da8619ef9afc3d71486d3887
grebson <grebciu@gmail.com>
2022-02-17 20:08:02 +0100

    1

--- player.py
+++ player.py
@@ -202,8 +202,8 @@ class Player(Entity):
         return base_damage + weapon_damage
 
     def energy_recovery(self):
-        if self.energy <= self.stats['energy']:
-            self.energy += 1
+        if self.energy < self.stats['energy']:
+            self.energy += 0.01 * self.stats['magic']
         else:
             self.energy = self.stats['energy']
 
