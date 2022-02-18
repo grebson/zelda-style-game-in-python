f8ed7223b84b01ecf528d4a122842359177eddb9
grebson <grebciu@gmail.com>
2022-02-18 19:58:49 +0100

    Another commit

--- magic.py
+++ magic.py
@@ -25,7 +25,7 @@ class MagicPlayer:
                 direction = pygame.math.Vector2(1,0)
             elif player.status.split('_')[0] == 'left':
                 direction = pygame.math.Vector2(-1,0)
-            elif player.stats.split('_')[0] == 'up':
+            elif player.status.split('_')[0] == 'up':
                 direction = pygame.math.Vector2(0,-1)
             else:
                 direction = pygame.math.Vector2(0,1)
@@ -35,4 +35,9 @@ class MagicPlayer:
                     offset_x = (direction.x * i) * TILESIZE
                     x = player.rect.centerx + offset_x + randint(-TILESIZE // 3, TILESIZE // 3)
                     y = player.rect.centery + randint(-TILESIZE // 3, TILESIZE // 3)
+                    self.animation_player.create_particles('flame', (x,y), groups)
+                else:
+                    offset_y = (direction.y * i) * TILESIZE
+                    x = player.rect.centerx + randint(-TILESIZE // 3, TILESIZE // 3)
+                    y = player.rect.centery + offset_y + randint(-TILESIZE // 3, TILESIZE // 3)
                     self.animation_player.create_particles('flame', (x,y), groups)
