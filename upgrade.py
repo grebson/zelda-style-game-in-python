e558ecdc65ae9fb1304b45ce562311278d5b39b5
grebson <grebciu@gmail.com>
2022-02-20 17:27:58 +0100

    hehe

--- upgrade.py
+++ upgrade.py
@@ -81,14 +81,23 @@ class Item:
         self.font = font
 
     def display_names(self,surface,name,cost,selected):
+        # color
+        color = TEXT_COLOR_SELECTED if selected else TEXT_COLOR
+
         # title
-        title_surf = self.font.render(name,False,TEXT_COLOR)
+        title_surf = self.font.render(name,False,color)
         title_rect = title_surf.get_rect(midtop = self.rect.midtop + pygame.math.Vector2(0,20))
         # cost
+        cost_surf = self.font.render(f'{int(cost)}',False,color)
+        cost_rect = cost_surf.get_rect(midbottom = self.rect.midbottom - pygame.math.Vector2(0,20))
 
         # draw
         surface.blit(title_surf,title_rect)
+        surface.blit(cost_surf,cost_rect)
 
     def display(self,surface,selection_num,name,value,max_value,cost):
+        if self.index == selection_num:
+            pass
+
         pygame.draw.rect(surface,UI_BG_COLOR,self.rect)
-        self.display_names(surface,name,cost,False)
+        self.display_names(surface,name,cost,self.index == selection_num)
