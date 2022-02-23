f31762e8059ec8431b56ec390e1d7da0408a1d39
grebson <grebciu@gmail.com>
2022-02-23 13:27:06 +0100

    Last commit, project finished

--- player.py
+++ player.py
@@ -8,7 +8,7 @@ class Player(Entity):
         super().__init__(groups)
         self.image = pygame.image.load('./images/player.png').convert_alpha()
         self.rect = self.image.get_rect(topleft = pos)
-        self.hitbox = self.rect.inflate(0,-26)
+        self.hitbox = self.rect.inflate(-6,HITBOX_OFFSET['player'])
 
         # images setup
         self.import_player_assets()
@@ -71,6 +71,10 @@ class Player(Entity):
         self.hurt_time = None
         self.invulnerability_duration = 500
 
+        # import a sound
+        self.weapon_attack_sound = pygame.mixer.Sound('./audio/sword.wav')
+        self.weapon_attack_sound.set_volume(0.4)
+
     def import_player_assets(self):
         character_path = './images/player/'
         self.animations = {
@@ -120,6 +124,7 @@ class Player(Entity):
                 self.attacking = True
                 self.attack_time = pygame.time.get_ticks()
                 self.create_attack()
+                self.weapon_attack_sound.play()
 
             # magic input
             if keys[pygame.K_LCTRL]:
